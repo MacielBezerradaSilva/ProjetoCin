d0c40361eb3c5bb6e8a7ce6e4912a58be1bbeecb
Flavio Henrique <flaviohenriqu@gmail.com>
2015-03-13 23:54:36 -0300

    Aperfeiçoamento da listagem de charters com implementação do botões para realização do CRUD.

--- ProjetoTCC/src/gui/ListCharters.java
+++ ProjetoTCC/src/gui/ListCharters.java
@@ -1,11 +1,14 @@
 package gui;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.sql.SQLException;
 
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JTable;
 
 import table.CharterTableModel;
@@ -17,6 +20,7 @@ public class ListCharters extends javax.swing.JPanel  {
     private JButton btnNew, btnSave, btnUpdate, btnRemove, btnCancel;
 	private CharterTableModel model;
 	private static JTable _table;
+	private static JPanel panelButtons;
 	
 	public ListCharters(){		
         ClassLoader loader = getClass().getClassLoader();
@@ -35,20 +39,29 @@ public class ListCharters extends javax.swing.JPanel  {
 		}
 	    _table = new JTable(model);	    
 	    
+	    panelButtons = new JPanel();
+        panelButtons.setBorder(BorderFactory.createEtchedBorder());
+	    
 	    this.add(_table, BorderLayout.CENTER);
-		this.add(btnNew, "gapleft 90");
-        this.add(btnCancel);
-        this.add(btnSave, "gap unrelated");
-        this.add(btnUpdate, "gap unrelated");
-        this.add(btnRemove);		
+        this.setBounds(5, 150, 480, 240);
+        this.setBorder(BorderFactory.createTitledBorder("Lista de Charters"));
+        
+		panelButtons.add(btnNew, "gapleft 90");
+		panelButtons.add(btnCancel);
+		panelButtons.add(btnSave, "gap unrelated");
+		panelButtons.add(btnUpdate, "gap unrelated");
+		panelButtons.add(btnRemove);
+		panelButtons.setBounds(5, 105, 480, 40);
 	}
 	
 	public static void main(String args[]) {
 		ListCharters panel = new ListCharters();
 		JFrame f = new JFrame();
-	    f.getContentPane().add(panel);
+	    f.add(panelButtons);
+	    f.add(panel);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	    
 	    f.pack();
+	    f.setMinimumSize(new Dimension(500,420));
 		f.setVisible(true);	    
 	}
 }
